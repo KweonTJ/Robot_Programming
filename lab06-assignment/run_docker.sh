025f4c54b93da2f3a052bf8f40d79c37e24ab3ac
KweonTJ <144107369+KweonTJ@users.noreply.github.com>
2025-11-07 00:41:11 +0900

    Add files via upload

---
+++ lab06-assignment/run_docker.sh
@@ -0,0 +1,11 @@
+docker run -it --privileged \
+    -e DISPLAY=$DISPLAY \
+    --env="QT_X11_NO_MITSHM=1" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /dev:/dev:rw \
+    --env=LOCAL_USER_ID="$(id -u)" \
+    --hostname $(hostname) \
+    --network host \
+    --name lab06_container kweontj/lab06:assignment bash
+    # 수정 이유 : 도커 파일을 사용되는 컴퓨터의 이름, 폴더명으로 변경
+    # 추가적으로 수업 과제와 겹치지 않게 하기위해 스크립트 파일 수정
